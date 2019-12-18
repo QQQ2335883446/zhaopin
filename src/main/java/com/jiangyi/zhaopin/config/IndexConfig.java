c527ec3851ca260e9eb89519c1ec87508104cb99
hmr <2335883446@qq.com>
2019-12-18 10:07:34 +0800

    添加项目启动自启浏览器

---
+++ src/main/java/com/jiangyi/zhaopin/config/IndexConfig.java
@@ -0,0 +1,26 @@
+package com.jiangyi.zhaopin.config;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.io.IOException;
+
+/**
+ * 项目启动
+ * 启动默认浏览器并访问登录页面
+ */
+@Configuration
+public class IndexConfig {
+    @EventListener({ApplicationReadyEvent.class})
+    void applicationReadyEvent() {
+        System.out.println("应用已经准备就绪 ... 启动浏览器");
+        String url = "http://localhost:9090/login";
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
