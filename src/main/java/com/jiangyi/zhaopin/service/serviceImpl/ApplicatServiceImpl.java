a48c3e9a326ef181e68319d0bb2d8f8982bacaa0
hmr <2335883446@qq.com>
2019-12-18 11:55:18 +0800

    添加登录注册页面

---
+++ src/main/java/com/jiangyi/zhaopin/service/serviceImpl/ApplicatServiceImpl.java
@@ -0,0 +1,56 @@
+package com.jiangyi.zhaopin.service.serviceImpl;
+
+import com.jiangyi.zhaopin.dao.ApplicantMapper;
+import com.jiangyi.zhaopin.model.Applicant;
+import com.jiangyi.zhaopin.service.ApplicatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 求职者服务层
+ */
+@Service
+public class ApplicatServiceImpl implements ApplicatService{
+    @Autowired
+    ApplicantMapper applicantMapper;
+
+    /**
+     * 通过密码登录
+     * @param applicant
+     * @return
+     */
+    @Override
+    public Applicant selectByName(Applicant applicant) {
+        return applicantMapper.selectByName(applicant);
+    }
+
+    /**
+     * 添加用户
+     * @param applicant
+     */
+    @Override
+    public void insertApp(Applicant applicant) {
+        applicantMapper.insertApp(applicant);
+    }
+
+    /**
+     * 获取用户
+     * 通过用户名
+     * @param applicant
+     * @return
+     */
+    @Override
+    public Applicant selectAll(Applicant applicant) {
+        return applicantMapper.selectAll(applicant);
+    }
+
+    /**
+     * 通过用户名
+     * 解锁账户
+     * @param applicant
+     */
+    @Override
+    public void updatauLock(Applicant applicant) {
+        applicantMapper.updatauLock(applicant);
+    }
+}
